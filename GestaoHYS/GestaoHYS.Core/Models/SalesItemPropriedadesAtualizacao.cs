e1aa799e7b16e8600f805314bba437a91385fee3
Amanda <asantosptc@gmail.com>
2021-07-20 02:01:50 -0300

    add

---
+++ GestaoHYS/GestaoHYS.Core/Models/SalesItemPropriedadesAtualizacao.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoHYS.Core.Models
+{
+    public abstract class SalesItemPropriedadesAtualizacao
+    {
+        [Column("unit")]
+        public string Unit { get; set; }
+
+
+   
+    }
+
+}
